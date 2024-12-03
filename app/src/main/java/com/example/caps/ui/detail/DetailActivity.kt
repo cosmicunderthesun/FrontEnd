1300a7b7f2dfd73dba4c7a78acb51f65a67f25fa
Azra <makotolacia@gmail.com>
2024-12-03 23:26:11 +0700

    moved to ui folder

--- app/src/main/java/com/example/caps/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/example/caps/ui/detail/DetailActivity.kt
@@ -1,10 +1,7 @@
-package com.example.caps.ui
+package com.example.caps.ui.detail
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.caps.R
 import com.google.android.material.appbar.CollapsingToolbarLayout
 
